e0d479c7eb473a4450bd5c46999606668d98e1c0
tomas77745 <tbarbieri@frba.utn.edu.ar>
2023-04-18 16:53:57 -0300

    Solución TP0

--- 00-HelloWorld/hello.c
+++ 00-HelloWorld/hello.c
@@ -1,1 +1,14 @@
-aaa
+#include <stdio.h>
+
+void crearArchivo()
+{
+    FILE *archivo;
+    archivo = fopen("output.txt", "w");
+    fprintf(archivo, "Hello world!");
+    fclose(archivo);
+}
+
+int main() {
+    crearArchivo();
+    return 0;
+}
